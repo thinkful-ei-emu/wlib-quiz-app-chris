49d4421f5d164ba8c815ac44a424e123a3dbba1d
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-06-18 09:21:59 -0400

    Adding comments to expalin Model and Renderer classes

--- src/TriviaApi.js
+++ src/TriviaApi.js
@@ -4,14 +4,13 @@ class TriviaApi {
   fetchQuestions(count) {
     const url = new URL(TriviaApi.BASE_URL);
     url.searchParams.set('amount', count);
-
+    console.log(url);
     return fetch(url)
       .then(res => {
         // if non-2xx response, reject promise with status text
         if (!res.ok) {
           return Promise.reject({ message: res.statusText });
         }
-
         // Otherwise, parse the json body
         return res.json();
       })
@@ -22,9 +21,12 @@ class TriviaApi {
         }
 
         // Otherwise, success! Return the data to automatically resolve the promise successfully.
+        console.log(data);
         return data;
       });
   }
 }
 
 export default TriviaApi;
+
+
--- src/lib/Renderer.js
+++ src/lib/Renderer.js
@@ -2,8 +2,18 @@ import $ from 'jquery';
 import Model from './Model';
 
 const renderers = [];
-
+/**
+ * Classes that extend Renderer define a 
+ * template and reactive behavior for all 
+ * instances. A renderer instance is 
+ * constructed with the following required 
+ * parameters:
+ * 
+ * @param {Model} - model - a model instance
+ * @param {string} - el - DOM selector string for a permanent DOM element
+ */
 class Renderer {
+  // Renderers are instantiated with a model and a root DOM node
   constructor(model, el) {
     if (!model || !(model instanceof Model) || !el) {
       throw new Error('Must instantiate with (1) instance of Model and (2) valid DOM selector. If using super(), make sure to pass those parameters in.');
@@ -12,7 +22,12 @@ class Renderer {
     this.model = model;
     this.model.bindView(this);
     this.$el = $(el);
-
+    /**
+     * Renderers require a template method
+     * This method MUST return an HTML string
+     * The template will typically use this.model
+     * to guide varied output.
+     */
     if (
       !this.template || 
       typeof this.template !== 'function' ||
@@ -20,6 +35,29 @@ class Renderer {
     ) {
       throw new Error('Classes that inherit Renderer REQUIRE a template() function that returns an HTML string');
     }
+
+    /** 
+     * Optional getEvents method
+     * This method must return an object. 
+     * Each property defines the event listener 
+     * bindings, where the key is a DOM event 
+     * name and delegate DOM selector 
+     * (space separated), and the value is the 
+     * name of an event handler function existing 
+     * on the same class. For example:
+     * 
+     * getEvents() {
+     *   return {
+     *     'click .add-item': 'handleAddItem'
+     *   }
+     * }
+     * 
+     * The above definition will add a click 
+     * listener to all children elements with 
+     * the add-item class, and invoke the 
+     * handleAddItem() function defined on 
+     * the same class.
+     */
 
     if (this.getEvents) {
       const events = this.getEvents();
@@ -39,7 +77,12 @@ class Renderer {
   renderAll() {
     renderers.forEach(renderer => renderer.render());
   }
-
+  /**
+   * By default, the return value of template() 
+   * will be placed in the root element on every 
+   * model update. You can override this method 
+   * if you want to finetune for the rendering cycle.
+   */
   render() {
     this.$el.html(this.template());
   }
